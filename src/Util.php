685a6c82b3b0d633fe088a74793220e10dbdee55
Hendrik Legge <admin@themepoint.de>
2022-10-06 20:19:18 +0200

    Run cs fixer

--- src/Util.php
+++ src/Util.php
@@ -1,8 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2022 ThemePoint
+ *
+ * @author Hendrik Legge <hendrik.legge@themepoint.de>
+ *
+ * @version 1.0.0
+ */
+
 namespace ThemePoint\Scheduler;
 
-class Util
+final class Util
 {
     public static function isValidTimeZone(string $timeZone): bool
     {
