1c479f8c42d19e7c25b082cd04dae551017d03c1
Hendrik Legge <admin@themepoint.de>
2022-10-09 22:55:18 +0200

    Run cs fixer

--- src/Timer.php
+++ src/Timer.php
@@ -1,8 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2022 Flexic-Systems
+ *
+ * @author Hendrik Legge <hendrik.legge@themepoint.de>
+ *
+ * @version 1.0.0
+ */
+
 namespace Flexic\Scheduler;
 
-class Timer
+final class Timer
 {
     private const NEXT_TICK = '+ 1 minute';
 
@@ -17,7 +27,7 @@ class Timer
         $next = $actual->setTime(
             (int) $actual->format('H'),
             (int) $actual->modify(self::NEXT_TICK)->format('i'),
-            0
+            0,
         );
 
         return $next->getTimestamp() - $actual->getTimestamp();
