9cf3ec0d9981211b3cb7fb05d98c19bb072dffe2
Hendrik Legge <admin@themepoint.de>
2022-12-29 19:24:09 +0100

    Added factory

---
+++ src/Interfaces/ScheduleEventFactoryInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2022 Flexic-Systems
+ *
+ * @author Hendrik Legge <hendrik.legge@themepoint.de>
+ *
+ * @version 1.0.0
+ */
+
+namespace Flexic\Scheduler\Interfaces;
+
+interface ScheduleEventFactoryInterface
+{
+    /**
+     * @return ScheduleEventInterface[]
+     */
+    public function create(): array;
+}
