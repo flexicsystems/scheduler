dac0c46f4c0613f886832c13273a13733bd072e0
Hendrik Legge <admin@themepoint.de>
2023-01-06 17:09:39 +0100

    Added new builder

--- src/Schedule.php
+++ src/Schedule.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace Flexic\Scheduler;
 
 use Cron;
+use Flexic\CronBuilder;
 use Flexic\Scheduler\Factory\CronExpressionFactory;
 use Flexic\Scheduler\Interfaces\ScheduleInterface;
 
@@ -28,9 +29,13 @@ final class Schedule implements ScheduleInterface
         $this->timezone = new \DateTimeZone(\date_default_timezone_get());
     }
 
-    public function cron(string $expression): self
+    public function cron(CronBuilder\CronBuilder|CronBuilder\Cron|string $expression): self
     {
-        $this->cron = CronExpressionFactory::create($expression);
+        if ($expression instanceof CronBuilder\CronBuilder) {
+            $expression = $expression->build();
+        }
+
+        $this->cron = CronExpressionFactory::create((string) $expression);
 
         return $this;
     }
@@ -56,108 +61,43 @@ final class Schedule implements ScheduleInterface
         return $this->cron;
     }
 
-    public function minute(string|int $minute): self
+    public function minute(CronBuilder\Expression\ExpressionInterface|string|int $minute): self
     {
         $this->cron->setPart(Cron\CronExpression::MINUTE, (string) $minute);
 
         return $this;
     }
 
-    public function minutes(string|int ...$minutes): self
-    {
-        $this->cron->setPart(Cron\CronExpression::MINUTE, (string) \implode(',', $minutes));
-
-        return $this;
-    }
-
-    public function minutesBetween(string|int $start, string|int $end): self
-    {
-        $this->cron->setPart(Cron\CronExpression::MINUTE, (string) \sprintf('%s-%s', $start, $end));
-
-        return $this;
-    }
-
-    public function hour(string|int $hour): self
+    public function hour(CronBuilder\Expression\ExpressionInterface|string|int $hour): self
     {
         $this->cron->setPart(Cron\CronExpression::HOUR, (string) $hour);
 
         return $this;
     }
 
-    public function hours(string|int ...$hours): self
-    {
-        $this->cron->setPart(Cron\CronExpression::HOUR, (string) \implode(',', $hours));
-
-        return $this;
-    }
-
-    public function hoursBetween(string|int $start, string|int $end): self
-    {
-        $this->cron->setPart(Cron\CronExpression::HOUR, (string) \sprintf('%s-%s', $start, $end));
-
-        return $this;
-    }
-
-    public function day(string|int $day): self
+    public function day(CronBuilder\Expression\ExpressionInterface|string|int $day): self
     {
         $this->cron->setPart(Cron\CronExpression::DAY, (string) $day);
 
         return $this;
     }
 
-    public function days(string|int ...$days): self
-    {
-        $this->cron->setPart(Cron\CronExpression::DAY, (string) \implode(',', $days));
-
-        return $this;
-    }
-
-    public function daysBetween(string|int $start, string|int $end): self
-    {
-        $this->cron->setPart(Cron\CronExpression::DAY, (string) \sprintf('%s-%s', $start, $end));
-
-        return $this;
-    }
-
-    public function month(string|int $month): self
+    public function month(CronBuilder\Expression\ExpressionInterface|string|int $month): self
     {
         $this->cron->setPart(Cron\CronExpression::MONTH, (string) $month);
 
         return $this;
     }
 
-    public function months(string|int ...$months): self
-    {
-        $this->cron->setPart(Cron\CronExpression::MONTH, (string) \implode(',', $months));
-
-        return $this;
-    }
-
-    public function monthsBetween(string|int $start, string|int $end): self
-    {
-        $this->cron->setPart(Cron\CronExpression::MONTH, (string) \sprintf('%s-%s', $start, $end));
-
-        return $this;
-    }
-
-    public function dayOfWeek(string|int $dayOfWeek): self
+    public function dayOfWeek(CronBuilder\Expression\ExpressionInterface|string|int $dayOfWeek): self
     {
         $this->cron->setPart(Cron\CronExpression::WEEKDAY, (string) $dayOfWeek);
 
         return $this;
     }
 
-    public function daysOfWeek(string|int ...$daysOfWeek): self
+    public function builder(): CronBuilder\CronBuilder
     {
-        $this->cron->setPart(Cron\CronExpression::WEEKDAY, (string) \implode(',', $daysOfWeek));
-
-        return $this;
-    }
-
-    public function daysOfWeekBetween(string|int $start, string|int $end): self
-    {
-        $this->cron->setPart(Cron\CronExpression::WEEKDAY, (string) \sprintf('%s-%s', $start, $end));
-
-        return $this;
+        return CronBuilder\CronBuilder::create();
     }
 }
