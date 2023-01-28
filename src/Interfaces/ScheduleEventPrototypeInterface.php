8edeaa6d4a2e07d5541e4e8394cce33ff0d276d5
Hendrik Legge <admin@themepoint.de>
2023-01-28 20:05:42 +0100

    Updated prototype

--- src/Interfaces/ScheduleEventPrototypeInterface.php
+++ src/Interfaces/ScheduleEventPrototypeInterface.php
@@ -4,5 +4,10 @@ namespace Flexic\Scheduler\Interfaces;
 
 interface ScheduleEventPrototypeInterface
 {
+    public function __invoke(): void;
 
+    /**
+     * Configure the schedule for schedule event.
+     */
+    public function configure(ScheduleInterface $schedule): void;
 }
