bd9071520abce2ed8e127a13742b0fd7d9b1511e
Hendrik Legge <admin@themepoint.de>
2023-01-29 13:31:16 +0100

    Run cs fixer

--- src/Exception/InvalidArgumentException.php
+++ src/Exception/InvalidArgumentException.php
@@ -1,7 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2023 Flexic-Systems
+ *
+ * @author Hendrik Legge <hendrik.legge@themepoint.de>
+ *
+ * @version 2.0.0
+ */
+
 namespace Flexic\Scheduler\Exception;
 
-class InvalidArgumentException extends \InvalidArgumentException
+final class InvalidArgumentException extends \InvalidArgumentException
 {
 }
